9e9de80d956de2e27384cb7e896d2ca387999650
Fernando Pimenta <fernandoatp1@gmail.com>
2026-04-24 16:39:53 -0300

    Realizada a atividade 09

--- Atividade09_Comissao_vendedor.js
+++ Atividade09_Comissao_vendedor.js
@@ -1,30 +1,23 @@
-/*
-Desafio
+function calcularComissao (vendas, meta){
+    if (vendas <= 0){
+        return "Vendas Inválidas"
+    }
+    else if (meta <= 0){
+        return "Meta Inválida"
+    }
 
-Crie uma função chamada calcularComissao que receba:
+    let valor = 0
 
-vendas
-meta
-Regras
-Se vendas <= 0, retorne:
-"Vendas inválidas"
-Se meta <= 0, retorne:
-"Meta inválida"
-Se vendas >= meta, comissão de 10% sobre vendas.
-Se vendas < meta, comissão de 3% sobre vendas.
-Saída esperada
+    if (vendas >= meta){
+        valor = vendas * 0.1 
+    }
+    else {
+        valor = vendas * 0.03
+    }
+    return `Comissão: R$ ${valor}`
+}
 
-Retorne:
-
-"Comissão: R$ X"
-Exemplos
-console.log(calcularComissao(10000, 8000)) // Comissão: R$ 1000
-console.log(calcularComissao(5000, 8000))  // Comissão: R$ 150
-console.log(calcularComissao(0, 8000))     // Vendas inválidas
-console.log(calcularComissao(5000, 0))     // Meta inválida
-Quero que você use
-const ou let
-if / else
-return
-validação antes do cálculo
-*/
+console.log(calcularComissao(10000, 8000))
+console.log(calcularComissao(5000, 8000))
+console.log(calcularComissao(0, 8000))
+console.log(calcularComissao(5000, 0))
