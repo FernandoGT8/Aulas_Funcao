e23ba926dc9053d32b94251502b68a8b4e1fc27b
Fernando <fernandoatp1@gmail.com>
2026-04-24 08:10:04 -0300

    Ajuste de formatação

--- Atividade07_Calcular_IMC.js
+++ Atividade07_Calcular_IMC.js
@@ -1,23 +1,23 @@
 function calcularIMC(peso, altura){
     const imc = peso / (altura * altura)
-        if (imc < 18.5){
-            return "Abaixo do peso"
-        }
-        else if (imc >= 18.5 && imc <= 24.9){
-            return "Peso normal"
-        }
-        else if (imc >= 25 && imc <= 29.9){
-            return "Excesso de peso"
-        }
-        else if (imc >= 30 && imc <= 34.9){
-            return "Obesidade classe I"
-        }
-        else if (imc >= 35 && imc <= 39.9){
-            return "Obesidade classe II"
-        }
-        else {
-            return "Obesidade classe III"
-        }
+    if (imc < 18.5){
+        return "Abaixo do peso"
+    }
+    else if (imc <= 24.9){
+        return "Peso normal"
+    }
+    else if (imc <= 29.9){
+        return "Excesso de peso"
+    }
+    else if (imc <= 34.9){
+        return "Obesidade classe I"
+    }
+    else if (imc <= 39.9){
+        return "Obesidade classe II"
+    }
+    else {
+        return "Obesidade classe III"
+    }
 }
 
 console.log(calcularIMC(50, 1.70))
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,5 +1,26 @@
-function calcularDesconto(preco){
-    return preço * 0.9
+function calcularIMC(peso, altura){
+    const imc = peso / (altura * altura)
+        if (imc < 18.5){
+            return "Abaixo do peso"
+        }
+        else if (imc <= 24.9){
+            return "Peso normal"
+        }
+        else if (imc <= 29.9){
+            return "Excesso de peso"
+        }
+        else if (imc <= 34.9){
+            return "Obesidade classe I"
+        }
+        else if (imc <= 39.9){
+            return "Obesidade classe II"
+        }
+        else {
+            return "Obesidade classe III"
+        }
 }
-const final = calcularDesconto(100)
-console.log(final)
+
+console.log(calcularIMC(50, 1.70))
+console.log(calcularIMC(68, 1.70))
+console.log(calcularIMC(85, 1.70))
+console.log(calcularIMC(110, 1.70))
