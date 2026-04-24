966cc905e7cadc6445f5009da5eea9a91c4fc88e
Fernando <fernandoatp1@gmail.com>
2026-04-24 12:39:06 -0300

    Adicionado destalhes da atividade

---
+++ Atividade09_Comissao_vendedor.js
@@ -0,0 +1,30 @@
+/*
+Desafio
+
+Crie uma função chamada calcularComissao que receba:
+
+vendas
+meta
+Regras
+Se vendas <= 0, retorne:
+"Vendas inválidas"
+Se meta <= 0, retorne:
+"Meta inválida"
+Se vendas >= meta, comissão de 10% sobre vendas.
+Se vendas < meta, comissão de 3% sobre vendas.
+Saída esperada
+
+Retorne:
+
+"Comissão: R$ X"
+Exemplos
+console.log(calcularComissao(10000, 8000)) // Comissão: R$ 1000
+console.log(calcularComissao(5000, 8000))  // Comissão: R$ 150
+console.log(calcularComissao(0, 8000))     // Vendas inválidas
+console.log(calcularComissao(5000, 0))     // Meta inválida
+Quero que você use
+const ou let
+if / else
+return
+validação antes do cálculo
+*/
