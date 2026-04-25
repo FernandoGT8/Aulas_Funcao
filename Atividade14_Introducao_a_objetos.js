d0c81b40b2c0cec16b605771f96ced675f01901f
Fernando Pimenta <fernandoatp1@gmail.com>
2026-04-25 12:38:29 -0300

    Adicionado atividade 14

---
+++ Atividade14_Introducao_a_objetos.js
@@ -0,0 +1,30 @@
+function exibirPerfil (usuario){
+
+    
+
+    let mensagemDeTexto = ""
+    if (usuario.idade < 18){
+        mensagemDeTexto = "Erro: Apenas maiores de idade"
+    }
+    else{
+        mensagemDeTexto = `O colaborador ${usuario.nome} tem ${usuario.idade} anos e ocupa o cargo de ${usuario.cargo}.`
+    }
+    return mensagemDeTexto
+}
+
+const funcionario1 = {
+
+    nome: "Fernando",
+    idade: 20,
+    cargo: "Estagiário Backend"
+}
+
+const funcionario2 = {
+
+    nome: "Lucas",
+    idade: 16,
+    cargo: "Aprendiz"
+}
+
+console.log(exibirPerfil(funcionario1))
+console.log(exibirPerfil(funcionario2))
