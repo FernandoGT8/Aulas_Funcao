947154beb52aab14e011f48ebc183baef3b083e8
Fernando <fernandoatp1@gmail.com>
2026-04-28 08:49:35 -0300

    Ajustado espaçamento da atividade 18

--- Atividade18_Array_de_objetos.js
+++ Atividade18_Array_de_objetos.js
@@ -1,57 +1,53 @@
-function finalizarPedido (pedido){
-    
-    if (pedido.valorProduto <= 0){
+function finalizarPedido(pedido, tamanhoNome) {
+    if (pedido.valorProduto <= 0) {
         return "Pedido inválido"
     }
-    
-    if (pedido.estadoDestino === ""){
+
+    if (pedido.estadoDestino === "") {
         return "Destino inválido"
     }
-    
+
     let valorDesconto = 0
 
-    if (pedido.cupom === "DESCONTO10"){
+    if (pedido.cupom === "DESCONTO10") {
         valorDesconto = pedido.valorProduto * 0.1
-    }
-    
-    else if (pedido.cupom === "DESCONTO20"){
+    } else if (pedido.cupom === "DESCONTO20") {
         valorDesconto = pedido.valorProduto * 0.2
     }
 
     let valorFrete = 0
 
-    if (pedido.valorProduto > 200){
+    if (pedido.valorProduto > 200) {
         valorFrete = 0
-    }
-
-    else if (pedido.estadoDestino === "SP"){
+    } else if (pedido.estadoDestino === "SP") {
         valorFrete = 15
-    }
-
-    else if (pedido.estadoDestino === "RJ"){
+    } else if (pedido.estadoDestino === "RJ") {
         valorFrete = 20
-    }
-
-    else {
+    } else {
         valorFrete = 30
     }
 
     const valorTotal = pedido.valorProduto - valorDesconto + valorFrete
 
-    return `Produto: ${pedido.nomeProduto} | Total: R$ ${valorTotal.toFixed(2)} | Frete: R$ ${valorFrete.toFixed(2)} | Cupom aplicado: ${pedido.cupom}`
+    const nomeFormatado = pedido.nomeProduto.padEnd(tamanhoNome)
+    const totalFormatado = valorTotal.toFixed(2).padStart(7)
+    const freteFormatado = valorFrete.toFixed(2).padStart(6)
+    const cupomFormatado = pedido.cupom.padEnd(10)
+
+    return `Produto: ${nomeFormatado} | Total: R$ ${totalFormatado} | Frete: R$ ${freteFormatado} | Cupom aplicado: ${cupomFormatado}`
 }
 
-function listarPedidosFinalizados (pedidos){
-    
+function listarPedidosFinalizados(pedidos) {
+    const maiorNome = Math.max(...pedidos.map(pedido => pedido.nomeProduto.length))
     const resultados = []
-    
-    for (let i = 0; i < pedidos.length; i++){
+
+    for (let i = 0; i < pedidos.length; i++) {
         const pedido = pedidos[i]
-        const resultado = finalizarPedido(pedido)
+        const resultado = finalizarPedido(pedido, maiorNome)
 
         resultados.push(resultado)
     }
-    
+
     return resultados
 }
 
