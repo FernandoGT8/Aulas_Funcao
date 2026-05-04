84bb7f8ecd05e55065878b8947cdd744c1f813d1
Fernando <fernandoatp1@gmail.com>
2026-05-04 12:17:08 -0300

    Atualização da atividade 20 (ainda não terminada)

--- Atividade20_Usando_map_filter_e_reduce.js
+++ Atividade20_Usando_map_filter_e_reduce.js
@@ -1,15 +1,7 @@
-const produtos = [
-    { nome: "Teclado", preco: 100, estoque: 5 },
-    { nome: "Mouse", preco: 50, estoque: 0 },
-    { nome: "Monitor", preco: 800, estoque: 2 },
-    { nome: "Cadeira", preco: 600, estoque: 10 }
-]
+const numeros = [5, 10, 15, 20]
 
-const resultado = produtos.filter(function(produto){
-    return produto.estoque < 5
+const total = numeros.reduce(function(acumulador, numero){
+    return acumulador + numero
+}, 0)
 
-}).map(function(produto){
-    return produto.nome
-})
-
-console.log(resultado)
+console.log(total)
